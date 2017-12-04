fd73e6dad2a56eb39fe9cb22f005596b8b505fcf
Brett Stephens <sbrett@student.ethz.ch>
2017-12-04 21:18:11 +0100

    added repo

---
+++ plotSteeringAngle.m
@@ -0,0 +1,103 @@
+% Plot steering angle vs PWM input using Encoder data 
+clc;
+clear;
+cd '/Users/BrettStephens/Documents/Zarc_Brett/VICON Measurements/';
+
+
+str_folder = 'negative PWM (inc3, start 87)';
+files = dir(str_folder);
+
+%parse each file 
+for j = 3:length(files)
+    delta = zeros(1,length(files)-2);
+    
+    str_file = files(j).name;
+    str = strcat(str_folder,'/',str_file);
+    data = csvread(str,1);
+    file = fopen(str,'r');
+    header = fgets(file);
+    fclose(file);
+    labels = getLabels(header);
+    
+    %extract labels
+    for i=1:length(labels)
+        str = [labels{i} '=data(:,i);'];
+       eval(str); 
+    end
+    
+    N = length(t);
+
+    Q = [qw qx qy qz];
+
+    eulerAngles = quat2eul(Q);
+    head = eulerAngles(:,1); %heading angle [rad]
+
+    dxW = zeros(N-1,1);
+    dyW = zeros(N-1,1);
+    dxB = zeros(N-1,1);
+    dyB = zeros(N-1,1);
+    yaw = zeros(N-1,1);
+    for i=2:N
+        dxW(i-1) = (x(i) - x(i-1)) / (t(i) - t(i-1)); %world frame v_x
+        dyW(i-1) = (y(i) - y(i-1)) / (t(i) - t(i-1)); %world frame v_y
+        yaw(i-1) = (head(i) - head(i-1)) / (t(i) - t(i-1)); %[rad/s]
+        R = [cos(head(i)) -sin(head(i)); sin(head(i)) cos(head(i))]; %rotation matrix
+        temp = R' * [dxW(i-1); dyW(i-1)];
+        dxB(i-1) = temp(1); % body frame v_x
+        dyB(i-1) = temp(2); % body frame v_y
+    end
+
+    d1 = designfilt('lowpassiir','FilterOrder',12, ...
+        'HalfPowerFrequency',0.15,'DesignMethod','butter');
+    dxB_f = filtfilt(d1,dxB);
+    dyB_f = filtfilt(d1,dyB);
+    
+    d2 = designfilt('lowpassiir','FilterOrder',12, ...
+        'HalfPowerFrequency',0.05,'DesignMethod','butter');
+    yaw_f = filtfilt(d2,yaw);
+    
+    %want to get a "static" v_x to calc. the steering angle (delta) using 
+    %the equation: delta = atan( (yaw*(Lf+Lr)) / (v_x) )
+       
+    %threshold = 0.03;
+    %res = 150; 
+    threshold = 0.17;
+    res = 10;
+    
+    for i = (res+1):length(dxB_f)
+        %find when v_x stops changing
+        if dxB_f(i) > 0.74 && (abs(dxB_f(i) - dxB_f(i-res))) < threshold
+            index_start = i; %starting index
+            for k = index_start:length(dxB_f)
+                if abs(dxB_f(k)-dxB_f(index_start)) > 0.05
+                    index_end = k; %ending index
+                    flag = 1;
+                    break
+                end
+            end
+        end
+        
+        if(flag == 1)
+            break
+        end
+    end
+    
+    %indeces of "constant" v_x
+    indeces = (index_start:1:index_end);
+    
+    t_const = t(indeces);
+    v_x = dxB_f(indeces);
+
+    figure();
+    plot(t(1:end-1), dxB_f);hold on;
+    plot(t_const,v_x,'LineWidth',2);
+    
+    v_x_avg = mean(v_x);
+    yaw_f_avg = mean(yaw_f(indeces));
+
+    %steering angle calc
+    Lf = 0.1698;
+    Lr = 0.1542;
+    delta(j) = atan((yaw_f_avg*(Lf+Lr))/(v_x_avg));
+    
+end
---
+++ visualizeVelocities.m
@@ -0,0 +1,90 @@
+%vextract velocities
+
+str_folder = '/Users/BrettStephens/Documents/Zarc_Brett/VICON Measurements/negative PWM (inc3, start 87)/';
+str_file = 'viconExampleOutput_23-11-2017 02:54:53.csv';
+str = strcat(str_folder,str_file);
+data = csvread(str,1);
+file = fopen(str,'r');
+header = fgets(file);
+fclose(file);
+labels = getLabels(header);
+
+
+for i=1:length(labels)
+    str = [labels{i} '=data(:,i);'];
+   eval(str); 
+end
+
+%% plot positins and velocities
+
+N = length(t);
+
+
+
+plot(x,y, 'Linewidth', 2);
+xlabel('x');
+xlabel('y');
+
+Q = [qw qx qy qz];
+
+eulerAngles = quat2eul(Q);
+yaw = eulerAngles(:,1); %heading angle [rad]
+
+dxW = zeros(N-1,1);
+dyW = zeros(N-1,1);
+dxB = zeros(N-1,1);
+dyB = zeros(N-1,1);
+r = zeros(N-1,1);
+for i=2:N
+    dxW(i-1) = (x(i) - x(i-1)) / (t(i) - t(i-1)); %world frame v_x
+    dyW(i-1) = (y(i) - y(i-1)) / (t(i) - t(i-1)); %world frame v_y
+    r(i-1) = (yaw(i) - yaw(i-1)) / (t(i) - t(i-1)); %[rad/s]
+    R = [cos(yaw(i)) -sin(yaw(i)); sin(yaw(i)) cos(yaw(i))]; %rotation matrix
+    temp = R' * [dxW(i-1); dyW(i-1)];
+    dxB(i-1) = temp(1); % body frame v_x
+    dyB(i-1) = temp(2); % body frame v_y
+end
+
+d1 = designfilt('lowpassiir','FilterOrder',12, ...
+    'HalfPowerFrequency',0.15,'DesignMethod','butter');
+
+%filter the data
+
+yaw_f = filtfilt(d1,yaw);
+dxB_f = filtfilt(d1,dxB);
+dyB_f = filtfilt(d1,dyB);
+d2 = designfilt('lowpassiir','FilterOrder',12, ...
+    'HalfPowerFrequency',0.05,'DesignMethod','butter');
+r_f = filtfilt(d2,r);
+
+subplot(3,1,1)
+plot(t,yaw,'Linewidth', 2);
+ylabel('heading');
+hold on
+subplot(3,1,2)
+plot(t(1:end-1),dxB_f,'Linewidth', 2);
+hold on
+plot(t(1:end-1),dyB_f,'Linewidth', 2);
+legend('V_x', 'V_y')
+subplot(3,1,3)
+plot(t(1:end-1), r_f);
+ylabel('yaw rate')
+hold on
+
+figure
+subplot(3,1,1)
+plot(t,yaw,'Linewidth', 2);
+hold on
+plot(t,yaw_f,'Linewidth', 2);
+subplot(3,1,2)
+plot(t(1:end-1),dxB,'Linewidth', 2);
+hold on
+plot(t(1:end-1),dyB,'Linewidth', 2);
+plot(t(1:end-1),dxB_f,'Linewidth', 2);
+grid on
+plot(t(1:end-1),dyB_f,'Linewidth', 2);
+legend('V_x', 'V_y')
+subplot(3,1,3)
+plot(t(1:end-1), r);
+hold on
+plot(t(1:end-1), r_f);
